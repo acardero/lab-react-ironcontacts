2a8f21c3af53763f338e1e29312cdc083d122e88
Alan Cardero <alan.cardero@gmail.com>
2019-03-25 21:01:51 -0400

    random now working

--- starter-code/src/CardContainer.js
+++ starter-code/src/CardContainer.js
@@ -1,17 +1,27 @@
 import React, { Component, Fragment } from 'react';
 import contacts from './contacts';
+import otherContacts from './contacts';
 import ContactCard from './ContactCard';
 import CoolButton from './CoolButton';
 
 class CardContainer extends Component {
     state = {contacts: contacts.slice(0,5)}
+
+    addRandomContact = () => {
+        const {contacts} = this.state;
+        const copyOfContacts = [...otherContacts];
+        const rest = copyOfContacts.splice(5);
+        const random = Math.floor(Math.random()* rest.length);
+        const newContact = rest[random];
+        this.setState({contacts: [...contacts, newContact] });
+    }
     
     render() {
         const {contacts} = this.state;
         return (
             <Fragment>
                 <h1>IronContacts</h1>
-                <CoolButton>Add Random Contact</CoolButton>
+                <CoolButton OnClick={this.addRandomContact} isInfo isMedium>Add Random Contact</CoolButton>
                 <table>
                     <tbody>
                         <tr>
